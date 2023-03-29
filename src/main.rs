f3c2313705f1b0c92cf4964356621bc4a566449f
james.zhang <james.zhang@arsyun.com>
2023-03-29 17:43:54 +0800

    covert one animation to gif

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{env, process::Command};
+use std::process::Command;
 
-use ffmpeg_next::format::context::Input;
-use teloxide::{net::Download, prelude::*, types::InputFile};
+use teloxide::{net::Download, prelude::*};
 use tokio::fs;
 #[tokio::main]
 async fn main() {
@@ -12,6 +11,37 @@ async fn run() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        if let Some(anime) = msg.animation() {
+            let file_id = anime.file.id.clone();
+            let file = bot.get_file(file_id.clone()).await?;
+            let file_name = format!("{}.mp4", file_id.clone());
+            let path = format!("animation/{}", file_name);
+            let result=fs::create_dir("animation").await;
+            if let Err(e)=result{
+                if e.kind()!=std::io::ErrorKind::AlreadyExists{
+                    println!("error:{}",e);
+                    return ResponseResult::Err(teloxide::RequestError::Io(e))
+                }
+            }
+            println!("downloading {}", &file_name);
+            let mut dst = fs::File::create(&path).await?;
+            bot.download_file(&file.path, &mut dst).await?;
+            let gif_name = &format!("{}.gif", &path);
+            let ffmpeg_args = vec![
+                "-i",
+                &path,
+                "-vf",
+                r"scale=320:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse",
+                gif_name,
+            ];
+            let mut cmd = Command::new("ffmpeg");
+            cmd.args(ffmpeg_args);
+            let output = cmd.output().expect("Failed to execute command");
+            println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+            fs::remove_file(&path).await.unwrap_or_else(|why| {
+                println!("! {:?}", why.kind());
+            });
+        }
         if let Some(text) = msg.text() {
             if text.starts_with("https://t.me/addstickers/") {
                 let pack_name = &text["https://t.me/addstickers/".len()..];
