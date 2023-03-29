c77c60a8b7c1e7756e3231f613498f320b96cf2b
james.zhang <james.zhang@arsyun.com>
2023-03-29 15:33:23 +0800

    convert mp4 to gif

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
+use std::{env, process::Command};
+
+use ffmpeg_next::format::context::Input;
 use teloxide::{net::Download, prelude::*, types::InputFile};
 use tokio::fs;
-
 #[tokio::main]
 async fn main() {
     run().await;
@@ -16,21 +17,39 @@ async fn run() {
                 let pack_name = &text["https://t.me/addstickers/".len()..];
 
                 let sticker_set = bot.get_sticker_set(pack_name).send().await.unwrap();
-                let set_name=sticker_set.name.clone();
+                let set_name = sticker_set.name.clone();
                 fs::create_dir(&set_name).await?;
-                for (key,sticker) in sticker_set.stickers.iter().enumerate() {
-                    
+                for (key, sticker) in sticker_set.stickers.iter().enumerate() {
                     let file_id = sticker.file.id.clone();
                     let file = bot.get_file(file_id.clone()).await?;
-                    let file_name=if sticker.is_animated()||sticker.is_video(){
-                        format!("{}_{}.mp4",&set_name,key)
-                    }else{
-                        format!("{}_{}.png",&set_name,key)
+                    let file_name = if sticker.is_animated() || sticker.is_video() {
+                        format!("{}_{}.mp4", &set_name, key)
+                    } else {
+                        format!("{}_{}.png", &set_name, key)
                     };
-                    let path=format!("{}/{}",&set_name,file_name);
-                    println!("downloading {}",&file_name);
-                    let mut dst = fs::File::create(path).await?;
+                    let path = format!("{}/{}", &set_name, file_name);
+                    println!("downloading {}", &file_name);
+                    let mut dst = fs::File::create(&path).await?;
                     bot.download_file(&file.path, &mut dst).await?;
+                    if sticker.is_animated() || sticker.is_video() {
+                        //转换成gif
+                        // ffmpeg -i animation.gif.mp4 -vf "scale=320:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" output.gif
+                        let gif_name = &format!("{}.gif", &path);
+                        let ffmpeg_args = vec![
+                            "-i",
+                            &path,
+                            "-vf",
+                            r"scale=320:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse",
+                            gif_name,
+                        ];
+                        let mut cmd = Command::new("ffmpeg");
+                        cmd.args(ffmpeg_args);
+                        let output = cmd.output().expect("Failed to execute command");
+                        println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+                        fs::remove_file(&path).await.unwrap_or_else(|why| {
+                            println!("! {:?}", why.kind());
+                        });
+                    }
                 }
                 println!("download done");
             }
